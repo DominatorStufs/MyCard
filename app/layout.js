01013dd177459a8ff71ca9fdf7591d2a946b3999
DominatorStufs <dpdangergameryt@gmail.com>
2025-02-17 09:53:24 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const poppins = Poppins({
 });
 export const metadata = {
   title: {
-    default: 'MyCars',
+    default: 'MyCard',
     template: '%s | MyCard',
   },
   description: 'Open-Source link in bio tool for content creaters, developers and enthusiasts.',
