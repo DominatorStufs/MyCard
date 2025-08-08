ff698c43acbec6a949dda38b304b87c4ee095081
DominatorStufs <dpdangergameryt@gmail.com>
2025-08-08 21:23:06 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,22 +3,25 @@ import Logo from "./Logo";
 import { ModeToggle } from "./ModeToggle";
 
 export default function Footer() {
-    return (
-        <footer className="px-8 md:px-20 lg:px-32 py-5 mt-3 grid gap-4">
-            <div className="flex justify-between">
-                <div>
-                    <Logo />
-                    <p className="text-xs">Built by <a href="https://github.com/DominatorStufs" className="hover:text-primary hover:underline">Dominator</a> codebase on <a className="hover:text-primary hover:underline" href="https://github.com/DominatorStufs/MyCard">github</a></p>
-                </div>
-                <div>
-                    <ModeToggle/>
-                </div>
-            </div>
-            <ul className="text-sm grid gap-1">
-                <li className="hover:underline hover:text-primary text-sm"><Link href="https://github.com/DominatorStufs/MyCard">Fix This Web</Link></li>
-                <li className="hover:underline hover:text-primary text-sm"><Link href="https://github.com/DominatorStufs/MyCard">Report An Issue</Link></li>
-            </ul>
-            <p className="text-xs">&copy; {new Date().getFullYear()} <Link href="/" className="text-primary hover:underline">MyCard</Link> All Rights Reserved.</p>
-        </footer>
-    )
+  return (
+    <footer className="px-6 h-32 flex items-center justify-center sm:px-10 md:px-30 text-center sm:text-left">
+      <p className="text-sm text-foreground/80 sm:text-base">
+        Built by{" "}
+        <a
+          className="text-foreground underline"
+          href="https://github.com/DominatorStufs"
+        >
+          r2hu1
+        </a>
+        . Source code available on{" "}
+        <a
+          className="text-foreground underline"
+          href="https://github.com/DominatorStufs/MyCard"
+        >
+          GitHub
+        </a>
+        .
+      </p>
+    </footer>
+  );
 }
